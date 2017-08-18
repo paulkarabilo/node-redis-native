3f2ca2a55d56c30ad47dc42e19daeee058c78945
Paul Karabilo <benizebe@gmail.com>
2017-08-18 19:45:53 +0200

    test

--- test.js
+++ test.js
@@ -2,16 +2,8 @@ let r = require('./index.js');
 
 let c = new r.Client();
 
-c.call("ping", function(err, res) {console.log(res); });
+c.call("ping", (err, res) => console.log(res));
 
-c.call("SET test 1", function(err, res) {
-    c.call("GET test", function(err, res) {
-        console.log(res);
-    });
-});
+c.call("SET test 1", (err, res) => c.call("GET test", (err, res) => console.log(res)));
 
-c.set("test1", "1", function (err, res) {
-    c.get("test1", function(err, res) {
-        console.log(res);
-    });
-});
+c.set("test1", 1, (err, res) => c.get("test1", (err, res) => console.log(res)));
